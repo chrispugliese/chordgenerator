9089b0b6af6e827e310f2d2d0b598475909bb393
chrispugliese <89790420+chrispugliese@users.noreply.github.com>
2025-07-02 16:31:37 -0600

    build maven quickstart

---
+++ chordgenerator/src/main/java/com/chordgenerator/app/App.java
@@ -0,0 +1,10 @@
+package com.chordgenerator.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
